c2f19f60a0e79666f73374c0e39dccb67b47aea5
codingscode <heronnestg@hotmail.com>
2021-08-09 20:29:40 -0300

    parte 19

--- 001/pomodoro-app/src/components/DialogC.vue
+++ 001/pomodoro-app/src/components/DialogC.vue
@@ -2,7 +2,16 @@
    
     <v-dialog v-model="dialog" persistent max-width="600px" >
        <v-card>
-          Eu sou um dialog 
+          Eu sou um dialog
+         <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="dialog = false" >
+              Fechar
+            </v-btn>
+            <v-btn color="blue darken-1" text @click="dialog = false" >
+              Salvar
+            </v-btn>
+         </v-card-actions>
        </v-card>
 
     </v-dialog>
